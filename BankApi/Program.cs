4739139430d8bbbdd315526323c7e54db7d7fa67
lesia <olesjamelnyk04@gmail.com>
2026-04-29 22:21:04 +0300

    Change database from InMemory to PostgreSQL

--- BankApi/Program.cs
+++ BankApi/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("ProdDb"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ITransferService, TransferService>();
 
